7ff07ea3951c06349e83d0b10cf0622bf341a265
Snigi13 <65667637+Snigi13@users.noreply.github.com>
2020-07-06 16:38:00 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -4,23 +4,24 @@
             'restitution':0.8,
             'friction':1.0,
             'density':1.0
-        }
-        this.body =Bodies.circle(this.x,this.y,this.radius,options);
+        } 
         this.x = x;
         this.y = y;
         this.radius=radius;
+        this.body =Bodies.circle(this.x,this.y,this.radius,options);
+       
         
         this.image = loadImage("paper.png");
-        this.body = height;
+        //this.body = height;
         World.add(world,this.body);
       }
       display(){
         push();
         translate(this.body.position.x, this.body.position.y);
-       // ellipseMode(RADIUN)  
+      // ellipseMode(RADIAN)  
         imageMode(CENTER);
         image(this.image, 0, 0,x,y);
-        ellipse(0,0,this.radius,this.radius)
+        //ellipse(0,0,this.radius,this.radius)
         pop();
       }
 }
